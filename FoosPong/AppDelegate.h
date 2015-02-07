5eb104b4106c439540ad23cd2b726f8d8e91f2e2
Derik <derikflanary@gmail.com>
2015-02-07 10:35:44 -0700

    initial commit

---
+++ FoosPong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FoosPong
+//
+//  Created by Derik Flanary on 2/7/15.
+//  Copyright (c) 2015 Vibe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
