fb059956f30133b3a483f0aa7604e0b578d515cf
Derik <derikflanary@gmail.com>
2015-02-11 15:25:04 -0700

    sets game score, winner

--- FoosPong/GameViewController.m
+++ FoosPong/GameViewController.m
@@ -8,8 +8,15 @@
 
 #import "GameViewController.h"
 #import <PKYStepper/PKYStepper.h>
+#import "ChoosePlayersViewController.h"
 
 @interface GameViewController ()
+
+@property (nonatomic, assign) float scoreToWin;
+@property (nonatomic, strong) NSString *playerOneName;
+@property (nonatomic, strong) NSString *playerTwoName;
+@property (nonatomic, strong) PKYStepper *playerOneStepper;
+@property (nonatomic, strong) PKYStepper *playerTwoStepper;
 
 @end
 
@@ -18,16 +25,62 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+    self.scoreToWin = 11;
+    self.playerOneName = @"Derik";
+    self.playerTwoName = @"Danny";
     
-    PKYStepper * playerOneStepper = [[PKYStepper alloc]initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, 100)];
-    playerOneStepper.valueChangedCallback = ^(PKYStepper *stepper, float count) {
-        stepper.countLabel.text = [NSString stringWithFormat:@"Points: %@", @(count)];
+    self.playerOneStepper = [[PKYStepper alloc]initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, 100)];
+    self.playerOneStepper.valueChangedCallback = ^(PKYStepper *stepper, float count) {
+        stepper.countLabel.text = [NSString stringWithFormat:@"%@", @(count)];
     };
-    [playerOneStepper setup];
-    [self.view addSubview:playerOneStepper];
-    // Do any additional setup after loading the view.
+    [self.playerOneStepper setup];
+    self.playerOneStepper.maximum = self.scoreToWin;
+    [self.view addSubview:self.playerOneStepper];
+    
+    self.playerTwoStepper = [[PKYStepper alloc]initWithFrame:CGRectMake(0, 300, self.view.frame.size.width, 100)];
+    self.playerTwoStepper.valueChangedCallback = ^(PKYStepper *stepper, float count) {
+        stepper.countLabel.text = [NSString stringWithFormat:@"%@", @(count)];
+    };
+        [self.playerTwoStepper setup];
+    self.playerTwoStepper.maximum = self.scoreToWin;
+        [self.view addSubview:self.playerTwoStepper];
+    
+    [self.playerOneStepper.countLabel addObserver:self forKeyPath:@"text" options:NSKeyValueObservingOptionNew context:NULL];
+    
+    [self.playerTwoStepper.countLabel addObserver:self forKeyPath:@"text" options:NSKeyValueObservingOptionNew context:NULL];
+    
+    
+       }
+
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    NSLog(@"label changed");
+    
+    if ([self.playerOneStepper.countLabel.text isEqualToString:@"11"]) {
+    UIAlertController *setTitleAlert = [UIAlertController alertControllerWithTitle:[NSString stringWithFormat:@"%@ Wins!", self.playerOneName] message:@"" preferredStyle:UIAlertControllerStyleAlert];
+    [setTitleAlert addAction:[UIAlertAction actionWithTitle:@"End Game" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+        [self.navigationController popViewControllerAnimated:YES];
+    }]];
+    [self presentViewController:setTitleAlert animated:YES completion:nil];
+    }
+    
+    if (self.playerTwoStepper.value == 11){
+        UIAlertController *setTitleAlert = [UIAlertController alertControllerWithTitle:[NSString stringWithFormat:@"%@ Wins!", self.playerTwoName] message:@"" preferredStyle:UIAlertControllerStyleAlert];
+        [setTitleAlert addAction:[UIAlertAction actionWithTitle:@"End Game" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+            [self.navigationController popViewControllerAnimated:YES];
+        }]];
+        [self presentViewController:setTitleAlert animated:YES completion:nil];
+    }// Do any additional setup after loading the view.
+
+   
 }
 
+- (void)dealloc {
+    
+    [self.playerOneStepper.countLabel removeObserver:self forKeyPath:@"text"];
+    [self.playerTwoStepper.countLabel removeObserver:self forKeyPath:@"text"];
+    //[super dealloc];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
