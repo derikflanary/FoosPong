4259374220909075668004d99bb872e1d4422c08
Derik <derikflanary@gmail.com>
2015-03-12 16:02:49 -0600

    adds a rules webview in settings

---
+++ FoosPong/RulesViewController.h
@@ -0,0 +1,13 @@
+//
+//  RulesViewController.h
+//  FoosPong
+//
+//  Created by Derik Flanary on 3/12/15.
+//  Copyright (c) 2015 Vibe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RulesViewController : UIViewController
+
+@end
