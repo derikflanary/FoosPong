c719d0e0195033db80492926dd8910604889e108
Derik <derikflanary@gmail.com>
2015-03-30 00:43:09 -0600

    one more color change

--- FoosPong/TeamGameViewController.m
+++ FoosPong/TeamGameViewController.m
@@ -55,7 +55,7 @@
      [NSDictionary dictionaryWithObjectsAndKeys:
       [UIFont fontWithName:@"Thonburi-Light" size:18],
       NSFontAttributeName, nil]];
-    [self.navigationController.navigationBar setTintColor:[UIColor mainBlack]];
+    [self.navigationController.navigationBar setTintColor:[UIColor darkColor]];
     [self.navigationController.navigationBar setBarTintColor:[UIColor transparentWhite]];
     
     UIImageView *background = [[UIImageView alloc]initWithImage:[UIImage mainBackgroundImage]];
